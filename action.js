314e766108175dd097f28df06e38bdc83d24befc
Pradeep Singh Bisht <65757644+PradeepSinghBisht@users.noreply.github.com>
2020-11-09 17:55:22 +0530

    Add files via upload

--- action.js
+++ action.js
@@ -2,79 +2,35 @@ var result = "";
 var temp = 0;
 var opt;
 var z = "";
+
 function function1(val){
     if (opt != '/' && opt != '*' && opt != '+' && opt != '-') {
         document.getElementById('number').value += val;
-        temp = document.getElementById('number').value;
-        console.log(temp);
     } else {
         z = z + val;
         document.getElementById('number').value = z;
-        temp = z;
     }
+    temp = document.getElementById('number').value;
 }
 
 function function2(val){
     z = "";
     opt1 = val;
-    console.log(opt1);
-    if (val == '+') {
-        if (result == ""){
-            check();
-        } else {
-            calculate(opt);
-        }
-          
-    } else if (val == '-') {
-        if (result == ""){
-            check();
-        } else {
-            calculate(opt);
-        }
-    } else if (val == '*') {
-        if (result == ""){
-            check();
-        } else {
-            calculate(opt);
-        }
-    } else if (val == '/') {
-        if (result == ""){
-            check();
-        } else {
-            calculate(opt);
-        }
-    }
+    result == "" ? result = temp : calculate(opt);
+    temp = "";
+    document.getElementById('number').value = result;
     opt = opt1; 
 } 
-
-function check() {
-    result = temp;
-    temp = "";
-    console.log(result);
-    document.getElementById('number').value = result;
-}
 
 function calculate(opt) {
     if (opt == '+') {
         result = parseInt(result) + parseInt(temp);
-        temp = "";
-        console.log(result);
-        document.getElementById('number').value = result;
     } else if (opt == '-') {
         result = parseInt(result) - parseInt(temp);
-        temp = "";
-        console.log(result);
-        document.getElementById('number').value = result;
     } else if (opt == '*') {
         result = parseInt(result) * parseInt(temp);
-        temp = "";
-        console.log(result);
-        document.getElementById('number').value = result;
     } else if (opt == '/') {
         result = parseInt(result) / parseInt(temp);
-        temp = "";
-        console.log(result);
-        document.getElementById('number').value = result;
     }
 }
 
@@ -86,6 +42,7 @@ function results(val) {
         opt = "";
     } else if (val == '=') {
         calculate(opt);
+        document.getElementById('number').value = result;
         opt = " ";
     }
 }
