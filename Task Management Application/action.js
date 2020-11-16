63acf2acad2ef5764edc3bfc25a952083c848172
Pradeep Singh Bisht <65757644+PradeepSinghBisht@users.noreply.github.com>
2020-11-16 13:22:52 +0530

    Add files via upload

---
+++ Task Management Application/action.js
@@ -0,0 +1,86 @@
+var todotask = [];
+var taskcompleted = [];
+
+function addToDoTask(){
+    todotask.push(document.getElementById('task').value);
+    console.log(todotask);
+    toDoDisplay();
+}
+
+function toDoDisplay() {
+    var html = '';
+    for (var i = 0; i < todotask.length; i++) {
+        html += '<li> <input type="checkbox" onclick="addTaskCompleted(\''+i+'\')" id="completed'+i+'">'+todotask[i]+
+                '<input type="text" id="edittodo'+i+'" readonly>'+
+                '<button type="submit" onclick="editToDoTask(\''+i+'\')" id="edit'+i+'">Edit</button>'+
+                '<button type="submit" onclick="updateToDoTask(\''+i+'\')" id="update'+i+'" class="abc">Update</button>'+
+                '<button type="submit" onclick="deleteToDoTask(\''+i+'\')" id="delete">Delete</button> </li>';
+    }
+    document.getElementById('todo').innerHTML = html;
+    document.getElementById('task').value = "";
+}
+
+function editToDoTask(i){
+    document.getElementById('edittodo'+i).value = todotask[i];
+    document.getElementById('edit'+i).style.display = "none";
+    document.getElementById('update'+i).style.display = "inline";
+    document.getElementById('edittodo'+i).readOnly = false;
+}
+
+function updateToDoTask(i){
+    todotask[i] = document.getElementById('edittodo'+i).value;
+    document.getElementById('update'+i).style.display = "none";
+    document.getElementById('edit'+i).style.display = "inline";
+    toDoDisplay();
+}
+
+function deleteToDoTask(i){
+    todotask.splice(i, 1);
+    toDoDisplay();
+}
+
+function addTaskCompleted(i) {
+    taskcompleted.push(todotask[i]);
+    deleteToDoTask(i);
+    console.log(todotask);
+    console.log(taskcompleted);       
+    taskCompletedDisplay();
+}
+
+function addToDo(i) {
+    todotask.push(taskcompleted[i]);
+    deleteTaskCompleted(i);   
+    toDoDisplay();
+}
+
+function taskCompletedDisplay() {
+    var html = '';
+    for (var i = 0; i < taskcompleted.length; i++) {
+        html += '<li> <input type="checkbox" onclick="addToDo(\''+i+'\')" id="completed'+i+'" checked>'+taskcompleted[i]+
+                '<input type="text" id="editcompleted'+i+'" readonly>'+
+                '<button type="submit" onclick="editTaskCompleted(\''+i+'\')" id="edittask'+i+'">Edit</button>'+
+                '<button type="submit" onclick="updateTaskCompleted(\''+i+'\')" id="updatetask'+i+'" class="abc">Update</button>'+
+                '<button type="submit" onclick="deleteTaskCompleted(\''+i+'\')" id="delete">Delete</button> </li>';
+    }
+    document.getElementById('completed').innerHTML = html;
+    document.getElementById('task').value = "";
+}
+
+function editTaskCompleted(i){
+    document.getElementById('editcompleted'+i).value = taskcompleted[i];
+    document.getElementById('edittask'+i).style.display = "none";
+    document.getElementById('updatetask'+i).style.display = "inline";
+    document.getElementById('editcompleted'+i).readOnly = false;
+}
+
+function updateTaskCompleted(i){
+    taskcompleted[i] = document.getElementById('editcompleted'+i).value;
+    document.getElementById('updatetask'+i).style.display = "none";
+    document.getElementById('edittask'+i).style.display = "inline";
+    taskCompletedDisplay();
+}
+
+function deleteTaskCompleted(i){
+    taskcompleted.splice(i, 1);
+    taskCompletedDisplay();
+}
